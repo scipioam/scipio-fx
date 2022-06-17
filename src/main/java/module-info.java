dfbebc214bf3d78e1b453b82526360dd25a71c4d
AlanM <as@git.com>
2022-06-17 17:10:55 +0800

    调整test资源的结构

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -14,11 +14,11 @@ module com.github.scipio.scipio_fx {
     exports com.github.ScipioAM.scipio_fx.utils;
     exports com.github.ScipioAM.scipio_fx.view;
 
-    opens com.github.ScipioAM.scipio_fx.test.util;
-    opens com.github.ScipioAM.scipio_fx.test;
-    opens com.github.ScipioAM.scipio_fx.test.bean;
-    opens com.github.ScipioAM.scipio_fx.test.original;
-    opens com.github.ScipioAM.scipio_fx.test.controllers;
-    opens com.github.ScipioAM.scipio_fx.test.threads;
+//    opens com.github.ScipioAM.scipio_fx.test.util;
+//    opens com.github.ScipioAM.scipio_fx.test;
+//    opens com.github.ScipioAM.scipio_fx.test.bean;
+//    opens com.github.ScipioAM.scipio_fx.test.original;
+//    opens com.github.ScipioAM.scipio_fx.test.controllers;
+//    opens com.github.ScipioAM.scipio_fx.test.threads;
 
 }
