ec20e699f443067243c5273e624e5192d7cb773f
AlanM <as@git.com>
2022-06-27 17:13:26 +0800

    MFXComboBoxSupport

--- src/main/java/com/github/ScipioAM/scipio_fx/combobox/mfx/MFXComboBoxSupport.java
+++ src/main/java/com/github/ScipioAM/scipio_fx/combobox/mfx/MFXComboBoxSupport.java
@@ -1,7 +1,25 @@
 package com.github.ScipioAM.scipio_fx.combobox.mfx;
 
+import io.github.palexdev.materialfx.controls.MFXComboBox;
+import io.github.palexdev.materialfx.utils.others.FunctionalStringConverter;
+import javafx.util.StringConverter;
+
+import java.util.Collection;
+import java.util.function.Function;
+
 /**
  * @since 2022/6/24
  */
 public class MFXComboBoxSupport {
+
+    public static <T> MFXComboBox<T> setup(MFXComboBox<T> comboBox, Collection<T> data, Function<T, String> displayFunc) {
+        if(comboBox == null) {
+            comboBox = new MFXComboBox<>();
+        }
+        comboBox.getItems().addAll(data);
+        StringConverter<T> converter = FunctionalStringConverter.to(item -> item == null ? "" : displayFunc.apply(item));
+        comboBox.setConverter(converter);
+        return comboBox;
+    }
+
 }
