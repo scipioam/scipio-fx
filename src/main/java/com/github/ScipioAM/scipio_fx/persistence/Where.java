50f0f1d68959f36b2ca7ce0202b021b3c00bbf05
AlanM <as@git.com>
2022-07-13 15:49:45 +0800

    优化Where

--- src/main/java/com/github/ScipioAM/scipio_fx/persistence/Where.java
+++ src/main/java/com/github/ScipioAM/scipio_fx/persistence/Where.java
@@ -14,16 +14,26 @@ public class Where {
     private final Map<String, WhereCondition> conditions = new LinkedHashMap<>();
 
     public String buildQueryJpql(String entityName) {
-        StringBuilder hql = new StringBuilder()
+        StringBuilder jpql = new StringBuilder()
                 .append("from ")
                 .append(entityName)
                 .append(" where ");
         for (Map.Entry<String, WhereCondition> entry : conditions.entrySet()) {
             WhereCondition condition = entry.getValue();
-            hql.append(entry.getKey()).append(condition.getSqlOperator()).append(condition.getValue()).append(" and ");
+
+            jpql.append(entry.getKey()).append(condition.getSqlOperator());
+
+            Object value = condition.getValue();
+            if (value instanceof String) {
+                jpql.append('\'').append(value).append('\'');
+            } else if (value != null) {
+                jpql.append(value);
+            }
+
+            jpql.append(" and ");
         }
-        hql.delete(hql.length() - 5, hql.length() - 1);
-        return hql.toString();
+        jpql.delete(jpql.length() - 6, jpql.length() - 1);
+        return jpql.toString();
     }
 
     public static Where build() {
@@ -118,6 +128,26 @@ public class Where {
      */
     public Where ge(String fieldName, Object value) {
         conditions.put(fieldName, new WhereCondition(value, SqlOperator.GREATER_EQUAL));
+        return this;
+    }
+
+    /**
+     * where语句：字段 is null
+     *
+     * @param fieldName 实体类字段名（不是DB字段名）
+     */
+    public Where isNull(String fieldName) {
+        conditions.put(fieldName, new WhereCondition(null, SqlOperator.GREATER_EQUAL));
+        return this;
+    }
+
+    /**
+     * where语句：字段 is not null
+     *
+     * @param fieldName 实体类字段名（不是DB字段名）
+     */
+    public Where isNotNull(String fieldName) {
+        conditions.put(fieldName, new WhereCondition(null, SqlOperator.GREATER_EQUAL));
         return this;
     }
 
