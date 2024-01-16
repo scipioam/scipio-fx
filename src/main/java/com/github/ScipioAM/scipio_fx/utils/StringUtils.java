c5a2d14afb883f2f6039a56f36f8b0eb009e8635
AlanScipio <as@git.com>
2024-01-16 15:48:38 +0800

    添加RandomUtils

--- src/main/java/com/github/ScipioAM/scipio_fx/utils/StringUtils.java
+++ src/main/java/com/github/ScipioAM/scipio_fx/utils/StringUtils.java
@@ -1,38 +1,57 @@
 package com.github.ScipioAM.scipio_fx.utils;
 
-import java.util.Random;
 import java.util.regex.Pattern;
 
 /**
- * Description: v1.2
- * 通用小工具类
- * Author: Alan Min
- * Create time: 2018/4/20
+ * 字符串工具类
+ *
+ * @author Alan Scipio
+ * create time: 2018/4/20
  */
 public class StringUtils {
 
-    private StringUtils() {
-    }
-
     /**
-     * 判断字符串是否不为空
+     * 判断字符串是否不为空(空格不算)
      */
     public static boolean isNotNull(String s) {
-        return ((s != null) && (!s.equals("")));
+        return (s != null) && (!"".equals(s));
     }
 
     /**
-     * 判断字符串是否为空
+     * 判断字符串是否为空(空格不算)
      */
     public static boolean isNull(String s) {
-        return ((s == null) || (s.equals("")));
+        return (s == null) || ("".equals(s));
+    }
+
+    /**
+     * 判断字符串是否不为空(空格不算)
+     */
+    public static boolean isNotBlank(String s) {
+        return !isBlank(s);
+    }
+
+    /**
+     * 判断字符串是否为空(空格不算)
+     */
+    public static boolean isBlank(String s) {
+        int strLen;
+        if (s == null || (strLen = s.length()) == 0) {
+            return true;
+        }
+        for (int i = 0; i < strLen; i++) {
+            if (!Character.isWhitespace(s.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
     }
 
     /**
      * 判断字符串是否为数字
      */
     public static boolean isNumeric(String str) {
-        Pattern pattern = Pattern.compile("\\d+(\\.\\d+)?");
+        Pattern pattern = Pattern.compile("^-?\\d+(\\.\\d+)?$");
         return pattern.matcher(str).matches();
     }
 
@@ -40,30 +59,32 @@ public class StringUtils {
      * 判断字符串是否为整数
      */
     public static boolean isIntNumeric(String str) {
-        Pattern pattern = Pattern.compile("\\d+");
+        Pattern pattern = Pattern.compile("^-?\\d+$");
         return pattern.matcher(str).matches();
     }
 
     /**
      * 判断字符串是否为英文字母
      */
-    public static boolean isLetter(String str) {
-        Pattern pattern = Pattern.compile("[a-zA-Z]+");
+    public static boolean isAlpha(String str) {
+        Pattern pattern = Pattern.compile("^[a-zA-Z]+$");
         return pattern.matcher(str).matches();
     }
 
     /**
      * 判断字符串是否为英文字母或数字
      */
-    public static boolean isLetterNumeric(String str) {
+    public static boolean isAlphaNumeric(String str) {
         Pattern pattern = Pattern.compile("^[a-z0-9A-Z]+$");
         return pattern.matcher(str).matches();
     }
 
     /**
      * 判断字符串是否为http的url
-     * 匹配诸如：http://regxlib.com | http://electronics.cnet.com/electronics/0-6342366-8-8994967-1.html
-     * 不匹配：www.yahoo.com
+     * <ul>
+     *     <li>匹配诸如：http://regxlib.com | http://electronics.cnet.com/electronics/0-6342366-8-8994967-1.html</li>
+     *     <li>不匹配：www.yahoo.com</li>
+     * </ul>
      */
     public static boolean isHttpUrl(String s) {
         Pattern pattern = Pattern.compile("(http|ftp|https)://[\\w\\-_]+(\\.[\\w\\-_]+)+([\\w\\-.,@?^=%&:/~+#]*[\\w\\-@?^=%&/~+#])?");
@@ -71,7 +92,27 @@ public class StringUtils {
     }
 
     /**
-     * 把byte转为二进制表示的字符串
+     * 去除首尾空格，兼容null入参
+     */
+    public static String trim(final String str) {
+        return str == null ? null : str.trim();
+    }
+
+    /**
+     * 比较字符串是否相等，支持null入参
+     */
+    public static boolean equals(final String cs1, final String cs2) {
+        if (cs1 == null || cs2 == null) {
+            return false;
+        }
+        if (cs1.length() != cs2.length()) {
+            return false;
+        }
+        return cs1.equals(cs2);
+    }
+
+    /**
+     * 把byte转为bit字符串
      */
     public static String byteToBitStr(byte b) {
         return ""
@@ -86,7 +127,7 @@ public class StringUtils {
     }
 
     /**
-     * 二进制表示的字符串转byte
+     * 把bit字符串转为byte
      */
     public static byte bitStrToByte(String bit) {
         int re, len;
@@ -109,22 +150,6 @@ public class StringUtils {
     }
 
     /**
-     * 获取随机字符串
-     *
-     * @param length 随机字符串长度
-     */
-    public static String getRandom(int length) {
-        String str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-        Random random = new Random();
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < length; i++) {
-            int number = random.nextInt(62);
-            sb.append(str.charAt(number));
-        }
-        return sb.toString();
-    }
-
-    /**
      * 字符串插入
      *
      * @param offset    插入位置的下标
@@ -132,7 +157,7 @@ public class StringUtils {
      * @param insertStr 要插入的字符串
      * @return 插入过的字符串
      */
-    public static String insertStr(int offset, String oldStr, String insertStr) {
+    public static String insert(int offset, String oldStr, String insertStr) {
         StringBuilder sb = new StringBuilder(oldStr);
         sb.insert(offset, insertStr);
         return sb.toString();
@@ -142,6 +167,9 @@ public class StringUtils {
      * 英文首字母转为大写
      */
     public String firstToUpper(String s) {
+        if (isNull(s)) {
+            return s;
+        }
         char[] sArr = s.toCharArray();
         int ascii0 = sArr[0];
         if (ascii0 >= 97 && ascii0 <= 122) {
@@ -154,6 +182,9 @@ public class StringUtils {
      * 英文首字母转为小写
      */
     public String firstToLower(String s) {
+        if (isNull(s)) {
+            return s;
+        }
         char[] sArr = s.toCharArray();
         int ascii0 = sArr[0];
         if (ascii0 >= 65 && ascii0 <= 90) {
